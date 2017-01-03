d8cfa30c09c8589bc44d1c5d5a7ba2dd518fef82
takaheroes <takaheroes@gmail.com>
2017-01-04 05:59:42 +0900

    ¥С¼¥¸¥ç¥ó¤ò´ޤà¥ѥ¹¤ò»ØÄꤷ¤Ƥ¤¤ë²սê¤ò²սê¤򥳥á¥ó¥ȥ¢¥¦¥È

--- .emacs.d/config/inits/40_yasnippet.el
+++ .emacs.d/config/inits/40_yasnippet.el
@@ -6,5 +6,5 @@
 	:ensure yasnippet)
 
 (yas-global-mode 1)
-(yas/load-directory "~/.emacs.d/elisp/elpa/yasnippet-20161201.1520/snippets")
+;;(yas/load-directory "~/.emacs.d/elisp/elpa/yasnippet-20161201.1520/snippets")
 
