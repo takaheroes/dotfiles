0150bf94b299a70a610ddabbc2510620788ff720
takaheroes <takaheroes@gmail.com>
2017-01-11 19:22:20 +0900

    [update]ddskkの設定ファイルをinitファイルに分離
    skkの設定をEmacs起動時に読み込まれるinit.elファイルとskk起動時に読み込まれるinitファイルに分離

--- .emacs.d/config/inits/50_ddskk.el
+++ .emacs.d/config/inits/50_ddskk.el
@@ -1,31 +1,16 @@
 ;;;
-;;; DDSKK$B$N@_Dj(B
+;;; DDSKKの設定
 ;;;
 
-;; $B<-=q%U%!%$%k$N%Q%9$r@_Dj$9$k(B
-(setq skk-large-jisyo "~/.emacs.d/etc/ddskk/dic/SKK-JISYO.L")
 
-;; Emacs$B5/F0;~$K(BInput Method$B$r(BDDSKK$B$K$9$k(B
+;; Emacs起動時にInput MethodをDDSKKにする
 (setq default-input-method "japanese-skk")
 
-;; DDSKK$B@_Dj%U%!%$%k$N%U%)%k%@$r@_Dj$9$k(B
+;; DDSKK設定ファイルのフォルダを設定する
 (setq skk-user-directory "~/.emacs.d/config/ddskk")
 
-;; $B<-=q$NJ8;z%3!<%I$r(Butf-8$B!"2~9T%3!<%I$r(BLF$B$K@_Dj(B
-;(setq skk-jisyo-code 'utf-8-unix)  ; $B$3$l$r@_Dj$9$k$H<-=qJQ49$G$-$J$$$N$G%3%a%s%H%"%&%H(B
+;; 辞書の文字コードをutf-8、改行コードをLFに設定
+;(setq skk-jisyo-code 'utf-8-unix)  ; これを設定すると辞書変換できないのでコメントアウト
 
-;; $BJQ49$N$H$-(BEnter$B$r2!$7$F$b3NDj$N$_$G2~9T$7$J$$(B
-(setq skk-egg-like-newline t)
-
-;; "$B!V(B"$BF~NO$7$?$i(B"$B!W(B"$B$b<+F0A^F~(B
-(setq skk-auto-insert-paren t)
-
-;; $BAw$j2>L>$,87L)$K@5$7$$8uJd$rM%@h$7$FI=<((B
-(setq skk-henkan-strict-okuri-precedence t)
-
-;; $B4A;zEPO?;~!"Aw$j2>L>$,87L)$K@5$7$$$+$r%A%'%C%/(B
-(setq skk-check-okurigana-on-touroku t)
-
-;; Emacs$B5/F0;~$K(BDDSKK$B$r%W%j%m!<%I$7(BDDSKK$B5/F0;~$N=hM}$rB.$/$9$k(B
+;; Emacs起動時にDDSKKをプリロードしDDSKK起動時の処理を速くする
 (setq skk-preload t)
-
