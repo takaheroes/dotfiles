ec641d83a4955699398bd87798830217e00cc353
takaheroes <takaheroes@gmail.com>
2017-01-11 16:50:22 +0900

    [update]dired-launchでバイナリファイルを紐付けられたアプリで起動できるよう修正
    dired-launchのxdg-openの設定はLinux向けであり、この設定により日本語を含まないファイルもうまく起動できなかったため設定を解除
    Emacsのシェルモードが日本語文字化けしてうまく起動できなかったため文字化けを解消するため設定を追加

--- .emacs.d/config/inits/04_language.el
+++ .emacs.d/config/inits/04_language.el
@@ -4,3 +4,5 @@
 
 ;; 言語を「日本語」に設定する
 (set-language-environment "Japanese")
+
+(setenv "LANG" "ja_JP.UTF-8")
--- .emacs.d/config/inits/33_dired_launch.el
+++ .emacs.d/config/inits/33_dired_launch.el
@@ -1,10 +1,6 @@
 ;;
-;; dired-launch$B$N@_Dj(B
+;; dired-launchの設定
 ;;
 
-;; mimeopen$B$,;H$($J$$?M$O(Bxdg-open$B$GBeMQ$9$k(B
-(setq dired-launch-mailcap-friend '("env" "xdg-open"))
-
-;; $B$3$l$G(Bdired-launch-mode$B$,M-8z$K$J$j(B[J]$B$,;H$($k(B
+;; dired-launch-modeを有効にする
 (dired-launch-enable)
-
